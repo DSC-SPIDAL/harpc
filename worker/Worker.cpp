c0c4dae4ce2e5bfa112e56394a798561775aa494
Ahmet Uyar <ahmetuyar@gmail.com>
2019-05-06 15:17:16 +0300

    Worker.h separated into .h and .cpp

---
+++ worker/Worker.cpp
@@ -0,0 +1,46 @@
+//
+// Created by auyar on 06.05.2019.
+//
+
+#include "Worker.h"
+
+using namespace std;
+using namespace harp;
+
+namespace harp::worker {
+
+    void Worker::init(int argc, char *argv[]) {
+        int provided;
+        MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+        if (provided < MPI_THREAD_MULTIPLE) {
+            printf("ERROR: The MPI library does not have full thread support\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        //MPI_Init(NULL, NULL);
+
+        MPI_Comm_size(MPI_COMM_WORLD, &this->worldSize);
+
+        MPI_Comm_rank(MPI_COMM_WORLD, &this->workerId);
+
+        this->initialized = true;
+    }
+
+    void Worker::start(int argc, char *argv[]) {
+        if (!this->initialized) {
+            printf("Worker should be initialized");
+            return;
+        }
+        com::Communicator comm(this->workerId, this->worldSize, this->comThreads);
+        this->execute(&comm, argc, argv);
+        MPI_Finalize();
+    }
+
+    void Worker::setCommThreads(int comThreads) {
+        this->comThreads = comThreads;
+    }
+
+    bool Worker::isMaster() {
+        return this->workerId == 0;
+    }
+
+} // end of namespace harp::worker
--- worker/Worker.h
+++ worker/Worker.h
@@ -9,46 +9,19 @@ namespace harp::worker {
     protected:
         int workerId;
         int worldSize;
-
         int comThreads = 1;//no of communication threads
-
         bool initialized = false;
+
     public:
-        void init(int argc, char *argv[]) {
-            int provided;
-            MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-            if (provided < MPI_THREAD_MULTIPLE) {
-                printf("ERROR: The MPI library does not have full thread support\n");
-                MPI_Abort(MPI_COMM_WORLD, 1);
-            }
-            //MPI_Init(NULL, NULL);
+        void init(int argc, char *argv[]);
 
-            MPI_Comm_size(MPI_COMM_WORLD, &this->worldSize);
-
-            MPI_Comm_rank(MPI_COMM_WORLD, &this->workerId);
-
-            this->initialized = true;
-        }
-
-        void start(int argc = 0, char *argv[] = NULL) {
-            if (!this->initialized) {
-                printf("Worker should be initialized");
-                return;
-            }
-            com::Communicator comm(this->workerId, this->worldSize, this->comThreads);
-            this->execute(&comm, argc, argv);
-            MPI_Finalize();
-        }
+        void start(int argc = 0, char *argv[] = NULL);
 
         virtual void execute(com::Communicator *comm, int argc = 0, char *argv[] = NULL) = 0;
 
-        void setCommThreads(int comThreads) {
-            this->comThreads = comThreads;
-        }
+        void setCommThreads(int comThreads);
 
-        bool isMaster() {
-            return this->workerId == 0;
-        }
+        bool isMaster();
     };
 }
 
