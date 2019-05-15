ef420b1165071cfa13ba590f485d2018ec977c03
Ahmet Uyar <ahmetuyar@gmail.com>
2019-05-15 11:50:23 +0300

    insignificant small changes

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ Table<TYPE> * createTable(int tableId, int maxPartitions, int maxPartitionSize, 
     srand(workerId + time(NULL));
 //    int numOfPartitions = 3;
     int numOfPartitions = rand() % maxPartitions;
-    cout << "number of partitions: " << numOfPartitions << endl;
+//    cout << "number of partitions: " << numOfPartitions << endl;
 
     for (int p = 0; p < numOfPartitions; p++) {
         int partitionSize = rand() % maxPartitionSize + 1;
@@ -61,10 +61,94 @@ class MyWorker : public harp::worker::Worker {
 
     void execute(Communicator *comm, int argc = 0, char *argv[] = NULL) override {
 
-        testGather(comm);
-//        testAllGather(comm);
-//        testAllReduce(comm);
 //        testBroadcast(comm);
+//        testAllReduce(comm);
+//        testGather(comm);
+        testAllGather(comm);
+    }
+
+    void testBroadcast(Communicator *comm) {
+
+        Table<int> * tab;
+
+        // populate the table for the first worker
+        // this table will be broadcasted to all
+        if (this->workerId == 0) {
+            tab = createTable<int>(0, 5, 20, 1000);
+            cout <<  "original table: " << endl;
+            printTable(workerId, tab);
+
+            // tables of other workers will be empty
+        } else {
+            tab = new Table<int>(0);
+        }
+
+        // broadcast from 0 to all
+//        comm->broadcastAsOneArray(tab, 0);
+        comm->broadcastAsPartitions(tab, 0);
+
+        printTable(workerId, tab);
+
+        tab->clear();
+    }
+
+    void testAllReduce(Communicator *comm) {
+
+        Table<int> * tab;
+        tab = createTable<int>(0, 3, 10, 1000);
+        printTable(workerId, tab);
+
+        // broadcast from 0 to all
+//        comm->allReduceAsPartitions(tab, MPI_SUM);
+        comm->allReduceAsOneArray(tab, MPI_SUM);
+
+        cout << "table after allReduce" << endl;
+        printTable(workerId, tab);
+
+        tab->clear();
+    }
+
+    void testGather(Communicator *comm) {
+
+        Table<double> * tab = createTable<double>(0, 4, 10, 1000, workerId);
+        printTable(workerId, tab);
+
+        int rootWorkerId = 3;
+//        auto tables = comm->gatherAsPartitions(tab, rootWorkerId);
+        auto tables = comm->gatherAsOneArray(tab, rootWorkerId);
+
+        if (workerId == rootWorkerId) {
+            cout << "tables after gather at worker: " << workerId  << endl;
+            for (auto * table : *tables) {
+                printTable(table->getId(), table);
+//                harp::util::print::printTable(table);
+            }
+        }
+
+        tab->clear();
+    }
+
+    void testAllGather(Communicator *comm) {
+
+        Table<double> * tab = createTable<double>(0, 4, 10, 1000, workerId);
+        printTable(workerId, tab);
+
+        // broadcast from 0 to all
+        auto tables = comm->allGatherAsOneArray(tab);
+//        auto tables = comm->allGatherAsPartitions(tab);
+
+        if (workerId == 1) {
+            cout << "tables after all gather at worker: " << workerId << endl;
+            for (auto * table : *tables) {
+                printTable(table->getId(), table);
+//                harp::util::print::printTable(table);
+            }
+        }
+
+        cout << "table after allGather" << endl;
+//        printTable(workerId, tab);
+
+        tab->clear();
     }
 
     void testRotate(Communicator *comm) {
@@ -100,149 +184,12 @@ class MyWorker : public harp::worker::Worker {
         tab->clear();
     }
 
-    void testBroadcast(Communicator *comm) {
 
-        Table<int> * tab;
-
-        // populate the table for the first worker
-        // this table will be broadcasted to all
-        if (this->workerId == 0) {
-            tab = createTable<int>(0, 10, 100, 1000);
-            cout <<  "original table: " << endl;
-            printTable(workerId, tab);
-
-        // tables of other workers will be empty
-        } else {
-            tab = new Table<int>(0);
-        }
-
-        // broadcast from 0 to all
-        comm->broadcastAsOneArray(tab, 0);
-//        comm->broadcastAsPartitions(tab, 0);
-
-        printTable(workerId, tab);
-
-        tab->clear();
-    }
-
-    void testAllGather(Communicator *comm) {
-
-        Table<double> * tab = createTable<double>(0, 4, 10, 1000, workerId);
-        printTable(workerId, tab);
-
-        // broadcast from 0 to all
-        auto tables = comm->allGatherAsOneArray(tab);
-//        auto tables = comm->allGatherAsPartitions(tab);
-
-        if (workerId == 1) {
-            cout << "tables after all gather:" << endl;
-            for (auto * table : *tables) {
-                harp::util::print::printTable(table);
-            }
-        }
-
-        cout << "table after allGather" << endl;
-//        printTable(workerId, tab);
-
-        tab->clear();
-    }
-
-    void testGather(Communicator *comm) {
-
-        Table<double> * tab = createTable<double>(0, 4, 10, 1000, workerId);
-        printTable(workerId, tab);
-
-        int rootWorkerId = 3;
-//        auto tables = comm->gatherAsPartitions(tab, rootWorkerId);
-        auto tables = comm->gatherAsOneArray(tab, rootWorkerId);
-
-        if (workerId == rootWorkerId) {
-            cout << "tables after gather:" << endl;
-            for (auto * table : *tables) {
-                printTable(table->getId(), table);
-//                harp::util::print::printTable(table);
-            }
-        }
-
-        tab->clear();
-    }
-
-    void testAllReduce(Communicator *comm) {
-
-        Table<int> * tab;
-        tab = createTable<int>(0, 3, 10, 1000);
-        printTable(workerId, tab);
-
-        // broadcast from 0 to all
-        comm->allReduceAsPartitions(tab, MPI_SUM);
-//        comm->allReduceAsOneArray(tab, MPI_SUM);
-
-        cout << "table after allReduce" << endl;
-        printTable(workerId, tab);
-
-        tab->clear();
-    }
+}; // end of MyWorker
 
 
-};
-
-
-void testTableInfo() {
-
-    auto * table = createTable<int>(1, 10, 100, 1000);
-    cout <<  "original table: " << endl;
-    printTable(0, table);
-
-    TableInfo * tableInfo = new TableInfo(table);
-
-    cout << "number of partitions: " << tableInfo->getNumberOfPartitions() << endl;
-    int * ids = tableInfo->getPartitionIDs();
-    int * sizes = tableInfo->getPartitionSizes();
-    for (int i = 0; i < tableInfo->getNumberOfPartitions(); ++i) {
-        cout << ids[i] << ": " << sizes[i] << ", " << endl;
-    }
-
-    int * sdata = tableInfo->serialize();
-    cout << "serialized tableinfo: " << endl;
-    for (int j = 0; j < tableInfo->getSerializedSize(); ++j) {
-        cout << sdata[j] << ", ";
-    }
-
-    TableInfo * tableInfo2 = TableInfo::deserialize(sdata);
-    cout << "deserialized number of partitions: " << tableInfo2->getNumberOfPartitions() << endl;
-    ids = tableInfo2->getPartitionIDs();
-    sizes = tableInfo2->getPartitionSizes();
-    for (int i = 0; i < tableInfo2->getNumberOfPartitions(); ++i) {
-        cout << ids[i] << ": " << sizes[i] << ", " << endl;
-    }
-}
-
-void testTableSerialization() {
-
-    auto * table = createTable<int>(0, 10, 100, 1000);
-    cout <<  "original table: " << endl;
-    printTable(0, table);
-
-    auto * serializedTable = table->serialize();
-    int serializedSize = table->getSerializedSize();
-
-    cout << "st: ";
-    for (int k = 0; k < serializedSize; ++k) {
-        cout << serializedTable[k] << ",";
-    }
-    cout << endl;
-
-    TableInfo * tableInfo = new TableInfo(table);
-    auto * table2 = TableInfo::deserializeTable(serializedTable, tableInfo);
-    cout <<  "deserialized table: " << endl;
-    printTable(0, table);
-}
 
 int main() {
-
-//    cout << "starting ..." << endl;
-//    testTableInfo();
-//    testTableSerialization();
 
     MyWorker worker;
     worker.init(0, nullptr);
